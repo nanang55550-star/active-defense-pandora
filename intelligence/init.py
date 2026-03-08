b41de9f3f3055d9b5bfc03a929c758032f8aa2f7
Nzr <nanang55550@gmail.com>
2026-03-09 05:27:30 +0700

    Create init.py for intelligence module
    Initialize intelligence module with imports and exports.

---
+++ intelligence/init.py
@@ -0,0 +1,12 @@
+"""
+Intelligence Module - Threat intelligence and broadcast
+Author: @nanang55550-star
+"""
+
+from intelligence.broadcast_manager import BroadcastManager
+from intelligence.attacker_tracker import AttackerTracker
+
+__all__ = [
+    'BroadcastManager',
+    'AttackerTracker'
+]
